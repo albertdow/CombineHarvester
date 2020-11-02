49bc13fb730e989abf97287a45e6108329052fcc
Aliaksei Raspiareza <rasp@mail.desy.de>
2020-11-02 13:20:43 +0100

    cleaned home dir

--- HTTSMCPDecays18/fitting_etau_htt_bkg_et_2016.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc6_amd64_gcc700
-cd /nfs/dust/cms/user/rasp/CMSSW/CMSSW_10_2_13/src
-cmsenv
-cd /nfs/dust/cms/user/rasp/CMSSW/CMSSW_10_2_13/src/CombineHarvester/HTTSMCPDecays18/
-./Fitting.csh etau htt_bkg_et_2016
